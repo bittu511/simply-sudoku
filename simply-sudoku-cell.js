643161d57d77f42c9f3755b36629918300cb13a1
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2017-07-27 11:49:23 +0530

    Plans added for custom cell element.

---
+++ simply-sudoku-cell.js
@@ -0,0 +1,9 @@
+/*NOTE
+* value     - integer   constrained to 0~9
+* disabled  - boolean
+* focus()
+* Clicking displays the radial menu centered on the element
+* Clicking on a radial menu digit triggers the `valueChanged` event
+* Dragging on a cell directly triggers `valueChanged`
+* Long pressing on cell triggers the `valueChanged` event with `0`
+ */
